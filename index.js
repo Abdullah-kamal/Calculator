d836c4589e31d6f8f1f47f1721c7d87fe36a3426
Abdullah.kamal <abdullahkamal207@gmail.com>
2026-02-15 19:33:06 +0530

    fixed bugs on code

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const keys = document.getElementsByClassName("key");
 const clearDisplay = document.getElementById("clearDisplay");
 let op = "-";
+let operator_count = 0;
+let old_operator = "";
+
 
 let num1 = 0;
 let num2 = 0;
@@ -35,34 +38,48 @@ function operate(num1, num2, op) {
 Array.from(keys).forEach((element) => {
   element.addEventListener("click", (event) => {
     if (element.innerHTML != "=") {
+        // is display value
       let num = Number(element.innerHTML);
       console.log(num);
+      // if num is not a operator, then we get num2
       if (!Object.is(num, NaN) && operator != "") {
         num2 = num2 * 10 + num;
         display.innerHTML += element.innerHTML;
       }
+      // if num1 is not a operator, this will work
       if (!Object.is(num, NaN) && operator == "") {
         num1 = num1 * 10 + num;
         display.innerHTML += element.innerHTML;
-      } else if (Object.is(num, NaN) && operator == "") {
+      } else if (Object.is(num, NaN)) {
+        if(operator_count == 1) old_operator = operator;
         operator = element.innerHTML;
         display.innerHTML += operator;
+        operator_count++;
       }
-    } else {
-        result = operate(num1, num2, operator);
-        console.log(result);
-        display.innerHTML = Math.floor(result);
+      console.log(operator.length, "old operator" + old_operator)
+      if(operator_count == 2){
+        result = operate(num1, num2, old_operator);
+        display.innerHTML = result+ " "+ operator + " ";
         num1 = result;
         num2 = 0;
-        operator = "";
+        console.log(result);
+        operator_count = 1;
+      }
+    } else {
+      result = operate(num1, num2, operator);
+      console.log(result);
+      display.innerHTML = Math.floor(result);
+      num1 = result;
+      num2 = 0;
+      operator = "";
     }
     console.log(num1, num2, operator);
   });
 });
 clearDisplay.addEventListener("click", (event) => {
-    display.innerHTML = "";
-    operator = "";
-    num1 = "";
-    num2 = "";
-})
+  display.innerHTML = "";
+  operator = "";
+  num1 = "";
+  num2 = "";
+});
 console.log(operate(num1, num2, operator));
